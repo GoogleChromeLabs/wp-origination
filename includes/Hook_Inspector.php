3e077a3dc8a86278ca8fb9e47988529639a26567
Weston Ruter <westonruter@google.com>
2019-01-02 17:00:55 -0800

    Purge references to the hook wrapper from the query call stack

--- includes/Hook_Inspector.php
+++ includes/Hook_Inspector.php
@@ -67,6 +67,8 @@ class Hook_Inspector {
 
 	/**
 	 * After hook.
+	 *
+	 * @throws \Exception If the stack was empty, which should not happen.
 	 */
 	function after_hook() {
 		global $wpdb;
@@ -78,8 +80,17 @@ class Hook_Inspector {
 
 		$hook_inspection->end_time = microtime( true );
 		if ( defined( 'SAVEQUERIES' ) && SAVEQUERIES && $wpdb->num_queries !== $hook_inspection->before_num_queries ) {
+			$search = 'Sourcery\Hook_Wrapper->Sourcery\{closure}, call_user_func_array, ';
+
 			$hook_inspection->query_indices = array();
 			foreach ( range( $hook_inspection->before_num_queries, $wpdb->num_queries - 1 ) as $query_index ) {
+
+				// Purge references to the hook wrapper from the query call stack.
+				foreach ( $wpdb->queries[ $query_index ] as &$query ) {
+					$query = str_replace( $search, '', $query );
+				}
+
+				// Flag this query as being associated with this hook instance.
 				if ( ! isset( $this->sourced_query_indices[ $query_index ] ) ) {
 					$hook_inspection->query_indices[]            = $query_index;
 					$this->sourced_query_indices[ $query_index ] = true;
