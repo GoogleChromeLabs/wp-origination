c7d595bf944199af906bbf78eea87e05c9ac3cd3
Weston Ruter <westonruter@google.com>
2019-01-09 12:04:05 -0800

    Use Composer autoloader, copying code from wp-feature-policy

--- includes/Plugin.php
+++ includes/Plugin.php
@@ -1,8 +1,10 @@
 <?php
 /**
- * Class Plugin.
+ * Class Google\WP_Sourcery\Plugin
  *
- * @package Google\WP_Sourcery
+ * @package Google\WP_Feature_Policy
+ * @license GNU General Public License, version 2
+ * @link    https://wordpress.org/plugins/feature-policy/
  */
 
 namespace Google\WP_Sourcery;
@@ -13,14 +15,27 @@ namespace Google\WP_Sourcery;
 class Plugin {
 
 	/**
-	 * Options.
+	 * Main instance of the plugin.
 	 *
-	 * @var array
+	 * @since 0.1.0
+	 * @var Plugin|null
 	 */
-	public $options = array(
-		'show_queries_cap'  => 'manage_options',
-		'show_all_data_cap' => 'manage_options',
-	);
+	protected static $instance = null;
+
+	/**
+	 * Absolute path to the plugin main file.
+	 *
+	 * @since 0.1.0
+	 * @var string
+	 */
+	protected $main_file;
+
+	/**
+	 * Capability required to reveal the database queries on the page.
+	 *
+	 * @var string
+	 */
+	public $show_queries_cap = 'manage_options';
 
 	/**
 	 * Instance of Hook_Inspector.
@@ -39,12 +54,61 @@ class Plugin {
 	/**
 	 * Plugin constructor.
 	 *
-	 * @param array $options Args.
+	 * @since 0.1.0
+	 *
+	 * @param string $main_file Absolute path to the plugin main file. This is primarily useful for plugins that subclass this class.
 	 */
-	public function __construct( $options = array() ) {
-		$this->options = array_merge(
-			$this->options,
-			$options
+	public function __construct( $main_file ) {
+		$this->main_file = $main_file;
+	}
+
+	/**
+	 * Gets the plugin basename, which consists of the plugin directory name and main file name.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @return string Plugin basename.
+	 */
+	public function basename() {
+		return plugin_basename( $this->main_file );
+	}
+
+	/**
+	 * Gets the absolute path for a path relative to the plugin directory.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param string $relative_path Optional. Relative path. Default '/'.
+	 * @return string Absolute path.
+	 */
+	public function path( $relative_path = '/' ) {
+		return plugin_dir_path( $this->main_file ) . ltrim( $relative_path, '/' );
+	}
+
+	/**
+	 * Gets the full URL for a path relative to the plugin directory.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param string $relative_path Optional. Relative path. Default '/'.
+	 * @return string Full URL.
+	 */
+	public function url( $relative_path = '/' ) {
+		return plugin_dir_url( $this->main_file ) . ltrim( $relative_path, '/' );
+	}
+
+	/**
+	 * Determine whether sourcery should run for the current request.
+	 *
+	 * @return bool
+	 */
+	public function should_run() {
+		return (
+			defined( 'WP_DEBUG' )
+			&&
+			WP_DEBUG
+			&&
+			isset( $_GET['sourcery'] ) // phpcs:ignore WordPress.Security.NonceVerification.Recommended, WordPress.Security.NonceVerification.NoNonceVerification
 		);
 	}
 
@@ -52,11 +116,14 @@ class Plugin {
 	 * Init.
 	 */
 	public function init() {
+		if ( ! $this->should_run() ) {
+			return;
+		}
 
 		$this->hook_inspector = new Hook_Inspector(
 			array(
 				'can_show_queries_callback' => function() {
-					return current_user_can( $this->options['show_queries_cap'] );
+					return current_user_can( $this->show_queries_cap );
 				},
 			)
 		);
@@ -82,6 +149,8 @@ class Plugin {
 
 	/**
 	 * Send Server-Timing headers.
+	 *
+	 * @todo Move this to another class.
 	 */
 	public function send_server_timing_headers() {
 		$entity_timings = array();
@@ -113,5 +182,35 @@ class Plugin {
 			$value .= sprintf( ';dur=%f', $timing * 1000 );
 			header( sprintf( 'Server-Timing: %s', $value ), false );
 		}
+	}
+
+	/**
+	 * Retrieves the main instance of the plugin.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @return Plugin Plugin main instance.
+	 */
+	public static function instance() {
+		return static::$instance;
+	}
+
+	/**
+	 * Loads the plugin main instance and initializes it.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param string $main_file Absolute path to the plugin main file.
+	 * @return bool True if the plugin main instance could be loaded, false otherwise.
+	 */
+	public static function load( $main_file ) {
+		if ( null !== static::$instance ) {
+			return false;
+		}
+
+		static::$instance = new static( $main_file );
+		static::$instance->init();
+
+		return true;
 	}
 }
--- sourcery.php
+++ sourcery.php
@@ -6,7 +6,7 @@
  * @author    Weston Ruter
  * @copyright 2019 Google
  * @license   GNU General Public License, version 2 (or later)
- * @link      https://wordpress.org/plugins/feature-policy/
+ * @link      https://wordpress.org/plugins/sourcery/
  *
  * @wordpress-plugin
  * Plugin Name: Sourcery
@@ -22,21 +22,89 @@
 
 /* This file must be parsable by PHP 5.2. */
 
-namespace Google\WP_Sourcery;
+/**
+ * Loads the plugin.
+ *
+ * @since 0.1.0
+ */
+function _google_wp_sourcery_load() {
+	if ( version_compare( phpversion(), '5.6', '<' ) ) {
+		add_action( 'admin_notices', '_google_wp_sourcery_display_php_version_notice' );
+		return;
+	}
 
-// @todo Composer autoload.
-require_once __DIR__ . '/includes/Plugin.php';
-require_once __DIR__ . '/includes/Hook_Inspection.php';
-require_once __DIR__ . '/includes/Hook_Inspector.php';
-require_once __DIR__ . '/includes/Hook_Wrapper.php';
+	if ( version_compare( get_bloginfo( 'version' ), '4.9', '<' ) ) {
+		add_action( 'admin_notices', '_google_wp_sourcery_display_wp_version_notice' );
+		return;
+	}
 
-global $sourcery_plugin;
-$sourcery_plugin = new Plugin(
-	array(
-		// @todo 'annotate_output' => isset( $_GET['sourcery_annotate_output'] ),
-	)
-);
+	if ( ! file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+		add_action( 'admin_notices', '_google_wp_sourcery_display_composer_install_requirement' );
+		return;
+	}
 
-if ( defined( 'WP_DEBUG' ) && WP_DEBUG && isset( $_GET['sourcery'] ) ) {
-	$sourcery_plugin->init();
+	require_once __DIR__ . '/vendor/autoload.php';
+
+	call_user_func( array( 'Google\\WP_Sourcery\\Plugin', 'load' ), __FILE__ );
 }
+
+/**
+ * Displays an admin notice about an unmet PHP version requirement.
+ *
+ * @since 0.1.0
+ */
+function _google_wp_sourcery_display_php_version_notice() {
+	?>
+	<div class="notice notice-error">
+		<p>
+			<?php
+			sprintf(
+				/* translators: 1: required version, 2: currently used version */
+				__( 'Sourcery requires at least PHP version %1$s. Your site is currently running on PHP %2$s.', 'sourcery' ),
+				'5.6',
+				phpversion()
+			);
+			?>
+		</p>
+	</div>
+	<?php
+}
+
+/**
+ * Displays an admin notice about an unmet WordPress version requirement.
+ *
+ * @since 0.1.0
+ */
+function _google_wp_sourcery_display_wp_version_notice() {
+	?>
+	<div class="notice notice-error">
+		<p>
+			<?php
+			sprintf(
+				/* translators: 1: required version, 2: currently used version */
+				__( 'Sourcery requires at least WordPress version %1$s. Your site is currently running on WordPress %2$s.', 'sourcery' ),
+				'4.9',
+				get_bloginfo( 'version' )
+			);
+			?>
+		</p>
+	</div>
+	<?php
+}
+
+/**
+ * Displays an admin notice about the need to run composer install.
+ *
+ * @since 0.1.0
+ */
+function _google_wp_sourcery_display_composer_install_requirement() {
+	?>
+	<div class="notice notice-error">
+		<p>
+			<?php esc_html_e( 'The Sourcery plugin appears to being run from source and requires `composer install` to complete the plugin\'s installation.', 'sourcery' ); ?>
+		</p>
+	</div>
+	<?php
+}
+
+_google_wp_sourcery_load();
