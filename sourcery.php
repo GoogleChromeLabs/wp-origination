bdb849b82dbfddfd8c9f6dc5b7a4af1c3143910c
Weston Ruter <westonruter@google.com>
2019-01-12 18:20:59 -0800

    Consolidate code for load error checking; add warning for WP-CLI

--- sourcery.php
+++ sourcery.php
@@ -23,85 +23,95 @@
 /* This file must be parsable by PHP 5.2. */
 
 /**
- * Loads the plugin.
+ * Errors encountered while loading the plugin.
+ *
+ * This has to be a global for the same of PHP 5.2.
+ *
+ * @var \WP_Error $_google_wp_sourcery_load_errors
+ */
+global $_google_wp_sourcery_load_errors;
+
+/**
+ * Load the plugin, making sure the required dependencies are met.
  *
  * @since 0.1.0
  */
 function _google_wp_sourcery_load() {
+	global $_google_wp_sourcery_load_errors;
+	$load_errors = new WP_Error();
+
 	if ( version_compare( phpversion(), '5.6', '<' ) ) {
-		add_action( 'admin_notices', '_google_wp_sourcery_display_php_version_notice' );
-		return;
+		$load_errors->add(
+			'php_version',
+			sprintf(
+				/* translators: 1: required PHP version, 2: currently used PHP version */
+				__( 'Plugin requires at least PHP version %1$s; your site is currently running on PHP %2$s.', 'sourcery' ),
+				'5.6',
+				phpversion()
+			)
+		);
 	}
 
 	if ( version_compare( get_bloginfo( 'version' ), '4.9', '<' ) ) {
-		add_action( 'admin_notices', '_google_wp_sourcery_display_wp_version_notice' );
-		return;
+		$load_errors->add(
+			'wp_version',
+			sprintf(
+				/* translators: 1: required WordPress version, 2: currently used WordPress version */
+				__( 'Plugin requires at least WordPress version %1$s; your site is currently running on WordPress %2$s.', 'sourcery' ),
+				'4.9',
+				get_bloginfo( 'version' )
+			)
+		);
 	}
 
 	if ( ! file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
-		add_action( 'admin_notices', '_google_wp_sourcery_display_composer_install_requirement' );
-		return;
-	}
-
-	require_once __DIR__ . '/vendor/autoload.php';
-
-	call_user_func( array( 'Google\\WP_Sourcery\\Plugin', 'load' ), __FILE__ );
-}
-
-/**
- * Displays an admin notice about an unmet PHP version requirement.
- *
- * @since 0.1.0
- */
-function _google_wp_sourcery_display_php_version_notice() {
-	?>
-	<div class="notice notice-error">
-		<p>
-			<?php
+		$load_errors->add(
+			'composer_install',
 			sprintf(
-				/* translators: 1: required version, 2: currently used version */
-				__( 'Sourcery requires at least PHP version %1$s. Your site is currently running on PHP %2$s.', 'sourcery' ),
-				'5.6',
-				phpversion()
-			);
-			?>
-		</p>
-	</div>
-	<?php
-}
-
-/**
- * Displays an admin notice about an unmet WordPress version requirement.
- *
- * @since 0.1.0
- */
-function _google_wp_sourcery_display_wp_version_notice() {
-	?>
-	<div class="notice notice-error">
-		<p>
-			<?php
-			sprintf(
-				/* translators: 1: required version, 2: currently used version */
-				__( 'Sourcery requires at least WordPress version %1$s. Your site is currently running on WordPress %2$s.', 'sourcery' ),
+				sprintf(
+					/* translators: %s is composer install command */
+					__( 'Plugin appears to be running from source and requires %s to complete the plugin\'s installation.', 'sourcery' ),
+					'<code>composer install</code>'
+				),
 				'4.9',
 				get_bloginfo( 'version' )
-			);
-			?>
-		</p>
-	</div>
-	<?php
+			)
+		);
+	}
+
+	if ( ! empty( $load_errors->errors ) ) {
+		$_google_wp_sourcery_load_errors = $load_errors;
+		add_action( 'admin_notices', '_google_wp_show_dependency_errors_admin_notice' );
+		if ( defined( 'WP_CLI' ) && WP_CLI ) {
+			$messages = array( __( 'Sourcery plugin unable to load.', 'sourcery' ) );
+			foreach ( array_keys( $load_errors->errors ) as $error_code ) {
+				$messages[] = $load_errors->get_error_message( $error_code );
+			}
+			$message = implode( ' ', $messages );
+			$message = str_replace( array( '<code>', '</code>' ), '`', $message );
+			WP_CLI::warning( $message );
+		}
+	} else {
+		require_once __DIR__ . '/vendor/autoload.php';
+		call_user_func( array( 'Google\\WP_Sourcery\\Plugin', 'load' ), __FILE__ );
+	}
 }
 
 /**
- * Displays an admin notice about the need to run composer install.
+ * Displays an admin notice about why the plugin is unable to load.
  *
  * @since 0.1.0
+ * @global \WP_Error $_google_wp_sourcery_load_errors
  */
-function _google_wp_sourcery_display_composer_install_requirement() {
+function _google_wp_show_dependency_errors_admin_notice() {
+	global $_google_wp_sourcery_load_errors;
 	?>
 	<div class="notice notice-error">
 		<p>
-			<?php esc_html_e( 'The Sourcery plugin appears to being run from source and requires `composer install` to complete the plugin\'s installation.', 'sourcery' ); ?>
+			<strong><?php esc_html_e( 'Sourcery plugin unable to load.', 'sourcery' ); ?></strong>
+			<?php foreach ( array_keys( $_google_wp_sourcery_load_errors->errors ) as $error_code ) : ?>
+				<?php echo wp_kses_post( $_google_wp_sourcery_load_errors->get_error_message( $error_code ) ); ?>
+			<?php endforeach; ?>
 		</p>
 	</div>
 	<?php
