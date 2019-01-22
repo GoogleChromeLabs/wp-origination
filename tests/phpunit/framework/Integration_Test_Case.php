51c65319999e713f746bcfc67c570d2b5098a0f1
Weston Ruter <westonruter@google.com>
2019-01-22 08:19:05 -0800

    Add integration test framework

---
+++ tests/phpunit/framework/Integration_Test_Case.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class Google\WP_Sourcery\Tests\PHPUnit\Framework\Integration_Test_Case
+ *
+ * @package   Google\WP_Sourcery
+ * @link      https://github.com/westonruter/wp-sourcery
+ * @license   GPL-2.0-or-later
+ * @copyright 2019 Google Inc.
+ */
+
+namespace Google\WP_Sourcery\Tests\PHPUnit\Framework;
+
+use WP_UnitTestCase;
+
+/**
+ * Class representing an integration test case.
+ */
+class Integration_Test_Case extends WP_UnitTestCase {
+
+}
---
+++ tests/phpunit/integration/Sample_Tests.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Class Google\WP_Sourcery\Tests\PHPUnit\Unit\Sample_Tests
+ *
+ * @package   Google\WP_Sourcery
+ * @link      https://github.com/westonruter/wp-sourcery
+ * @license   GPL-2.0-or-later
+ * @copyright 2019 Google Inc.
+ */
+
+namespace Google\WP_Sourcery\Tests\PHPUnit\Unit;
+
+use Google\WP_Sourcery\Tests\PHPUnit\Framework\Integration_Test_Case;
+
+/**
+ * Class containing a sample test.
+ */
+class Sample_Tests extends Integration_Test_Case {
+
+	/**
+	 * Performs a sample test.
+	 */
+	public function testNothingUseful() {
+		$this->assertTrue( true );
+	}
+}
